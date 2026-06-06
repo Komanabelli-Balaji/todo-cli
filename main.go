e537fd2446883c5601032172b6bd4098d704650c
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-06-06 07:43:01 +0000

    inplemented storage

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 
 	todos.add("Add some features")
 	todos.add("Finish the project")
 	todos.toggle(0)
 	todos.print()
 
-	todos.delete(0)
-	todos.print()
+	storage.Save(todos)
 }
